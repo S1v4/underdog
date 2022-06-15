7d532f669e44992efc22f79f3ac8e876c499c5e1
“s1v4” <hdao61@gmail.com>
2022-06-15 15:07:35 -0700

    remove vcr setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,5 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
-require 'vcr_setup'
 
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
